e14be961129e7e461898cbba260ec7db940577dc
meladark <mikedark@yandex.ru>
2017-10-28 12:12:53 +0300

    Config

--- tb/config.py
+++ tb/config.py
@@ -1,10 +1,9 @@
 class Config:
-	def __init__(self, args):
-		self.args = args
 
-	def value(self, name):
-		# @todo #35 А давайте пока для простоты передадим токен
-		#  в параметрах командной строки, пусть будет первым параметром
-		#  но запрос нужно делать по ключу. Ключ для токена будет 'telegram.token'
-		#  А список аргументов командной строки содержится в self.args
-		return None
+	def __init__(self, args):
+	self.args = args
+	print("hello world")
+
+def value(self, name):
+
+	return None
